6678e16cf7d1299217422de1ab8bd6331e95477d
Manas Sharma <officalmanassharma@gmail.com>
2025-11-04 08:55:43 +0530

    Next Greater element in Right Done

---
+++ src/NextGreaterElementInRightNGER.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Stack;
+
+public class NextGreaterElementInRightNGER {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>(Collections.nCopies(n,-1));
+        for(int i =n-1;i>=0;i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                ans.set(i, stack.peek());
+            }
+            stack.push(arr[i]);
+        }return ans;
+    }
+}
