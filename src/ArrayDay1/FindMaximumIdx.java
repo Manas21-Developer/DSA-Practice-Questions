abc501061e8389bf3926ad4477f8a4378232ea94
Manas Sharma <officalmanassharma@gmail.com>
2025-11-05 19:00:52 +0530

    updated version of finding maximum ele

--- src/ArrayDay1/FindMaximumIdx.java
+++ src/ArrayDay1/FindMaximumIdx.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
 public class FindMaximumIdx {
-    public static void main(String[] args) throws Throwable {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;++i){
-            arr[i] = sc.nextInt();
-        }
-        int ans=ArrayProblem1(n,arr);
-        System.out.println(ans);
+    public static void main(String[] args) {
+        // Define the array directly
+        int[] arr = {10, 45, 23, 89, 12};
+        int n = arr.length;
+
+        // Call the function to find index of maximum element
+        int ans = ArrayProblem1(n, arr);
+
+        // Print results
+        System.out.println("Array: " + Arrays.toString(arr));
+        System.out.println("Index of Maximum Element: " + ans);
+        System.out.println("Maximum Element: " + arr[ans]);
     }
 
+    // Function to find index of maximum element
     public static int ArrayProblem1(int n, int[] arr) {
         int idx = 0;
         long max = Integer.MIN_VALUE;
-        for (int i = 0 ; i<n;i++)
-        {
-            if(arr[i] >max)
-            {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
                 idx = i;
             }
-        }return idx;
+        }
+        return idx;
     }
 }
