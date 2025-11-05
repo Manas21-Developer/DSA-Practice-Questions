9e9c265958075ad2cc8c2345d51bed716eabf6fc
Manas Sharma <officalmanassharma@gmail.com>
2025-11-05 18:38:30 +0530

    Find the maximum ele in the array and return the idx of that ele

---
+++ src/ArrayDay1/FindMaximumIdx.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class FindMaximumIdx {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;++i){
+            arr[i] = sc.nextInt();
+        }
+        int ans=ArrayProblem1(n,arr);
+        System.out.println(ans);
+    }
+
+    public static int ArrayProblem1(int n, int[] arr) {
+        int idx = 0;
+        long max = Integer.MIN_VALUE;
+        for (int i = 0 ; i<n;i++)
+        {
+            if(arr[i] >max)
+            {
+                max = arr[i];
+                idx = i;
+            }
+        }return idx;
+    }
+}
