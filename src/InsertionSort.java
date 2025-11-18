d7281f4b4d7eb8015027c3f55454f6774e641561
Manas Sharma <officalmanassharma@gmail.com>
2025-11-18 20:05:46 +0530

    Insertion Sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public void helper (int [] arr , int n){
+        for(int i =1 ; i<n;i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];// shift the ele into right .
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
