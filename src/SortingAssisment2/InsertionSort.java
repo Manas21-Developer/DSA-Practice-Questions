0a5587fb4213ed2a70d91c4d0d4e10cc872d5f84
Manas Sharma <officalmanassharma@gmail.com>
2025-11-19 16:39:34 +0530

    Push the code which help me to perform the insertion sort

---
+++ src/SortingAssisment2/InsertionSort.java
@@ -0,0 +1,35 @@
+package SortingAssisment2;
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int array[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            array[i]= sc.nextInt();
+        }
+
+        insertionSort(array,n);
+        for (int i = 0 ;i<n;i++)
+        {
+            System.out.print(array[i] +" ");
+        }
+    }
+    static void insertionSort(int arr[], int n)
+    {
+        for (int i = 1 ; i<n ; i++)
+        {
+            int key = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] >key)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
+
