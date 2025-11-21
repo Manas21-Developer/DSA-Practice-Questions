52b05eb7db77b7addbb199490bdd9fe9186fc0a3
Manas Sharma <officalmanassharma@gmail.com>
2025-11-22 00:44:30 +0530

    Fun that help me to find peak ele

--- src/BinarySearch2/PeakEle.java
+++ src/BinarySearch2/PeakEle.java
@@ -3,7 +3,8 @@ package BinarySearch2;
 public class PeakEle {
     public int findPeak(int arr[], int n)
     {
-        int s= 0 ; int e =n-1;
+        int s= 0 ;
+        int e =n-1;
         while(s<=e)
         {
             int mid = s+ (e-s)/2;
